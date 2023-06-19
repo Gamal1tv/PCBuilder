446d8246bc444c966c1b5cd561f57fa8ff2df50a
Gamal1tv <115107398+Gamal1tv@users.noreply.github.com>
2023-06-19 16:59:27 -0500

    Add files via upload

---
+++ Panel Practise/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Panel_Practise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btnFile);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(75, 22);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnFile
+            // 
+            this.btnFile.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnFile.Location = new System.Drawing.Point(0, 0);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 22);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.TabStop = false;
+            this.btnFile.Text = "File";
+            this.btnFile.UseVisualStyleBackColor = false;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(0, 40);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 22);
+            this.btn3.TabIndex = 1;
+            this.btn3.Text = "Open";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(0, 20);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 22);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "New";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnEdit.Location = new System.Drawing.Point(0, 0);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 22);
+            this.btnEdit.TabIndex = 0;
+            this.btnEdit.TabStop = false;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btn5);
+            this.panel2.Controls.Add(this.btn6);
+            this.panel2.Controls.Add(this.btnEdit);
+            this.panel2.Location = new System.Drawing.Point(12, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(75, 22);
+            this.panel2.TabIndex = 3;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(0, 20);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 22);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Cut";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(0, 40);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 22);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "Copy";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 53);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 22);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Panel Practise/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Panel_Practise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+
+            //Sets default Locations and sizes for all panels
+            panel1.Location = new Point(12, 12);
+            panel1.Size = new Size(75, 22);
+            panel2.Location = new Point(12, 32);
+            panel2.Size = new Size(75, 22);
+            btnExit.Location = new Point(12, 53);
+            
+        }
+
+        private void btnFile_Click(object sender, EventArgs e) //execute when btnFile is clicked
+        {
+            if (panel1.Size.Width == 75 && panel1.Size.Height == 22 && panel2.Size.Width == 75 && panel2.Size.Height == 22) //If panel 1 and panel 2 are at default size execute:
+            {
+                panel1.Size = new Size(75, 62); //change panel 1 size
+                panel2.Location = new Point(12, 72); //Change panel 2 location
+                btnExit.Location = new Point(12, 92);
+            }
+            else if()//if panel 1 is not at default size execute:
+            {
+                panel1.Size = new Size(75, 22); //change panel 1 size back to default
+                panel2.Location = new Point(12, 32);//change panel 2 location back to default
+                btnExit.Location = new Point(12, 53);
+            }
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e) //execute when btnEdit is clicked
+        {
+            if (panel2.Size.Width == 75 && panel2.Size.Height == 22) //If panel 2 is at defautl size execute:
+            {
+                panel2.Size = new Size(75, 62); //change panel 2 size
+                btnExit.Location = new Point(12, 102);
+            }
+            else if () //if panel 2 is not at default execute:
+            {
+                panel2.Size = new Size(75, 22); // change panel 2 size back to default
+                btnExit.Location = new Point(12, 53);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
